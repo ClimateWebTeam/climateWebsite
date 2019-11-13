3ca2f93ff434c84a76b4c07eaaf5e241a53d495f
Ben <bassbencooper999@gmail.com>
2019-11-13 11:17:24 +0000

    Added lots of server side changes to run this open terminal at working dir and type 'node index.js' then go to localhost:3000 in browser to test

--- JS/client.js
+++ JS/client.js
@@ -1,3 +1,37 @@
+let form = document.querySelector('form');
+
+form.onsubmit = sendData;
+
+function sendData(e){
+	e.preventDefault();
+	
+	//Possibly validate on client side here
+	
+	let formData = new FormData(form);
+	
+	let Params = {
+		headers: {
+			'Content-type': 'application/json'
+		},
+		body: JSON..stringify({
+			name: formData.get('name')
+		}),
+		method: "POST"
+	}
+	
+	fetch('http://localhost:3000/formData', Params)
+	.then(response => response.json())
+	.then(data => {
+		
+		console.log(data);
+		
+	})
+	.catch(err => console.log(err))
+	
+}
+
+
+
 var body = document.getElementsByTagName('body')[0];
 
 function toggleForm() {
--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const bodyParser = require('body-parser');
+
+let urlencoded = bodyParser.urlencoded({extended: false});
+
+app.use(bodyParser.json());
+app.use(urlencoded);
 
 app.use(express.static(__dirname + '/'))
 
@@ -10,5 +16,11 @@ app.get('/', (request, response) => {
 	response.sendFile(path.join(__dirname + '/index.html'));
 	
 });
+
+app.post('/formdata', (request, response) => {
+	
+	console.log(request.body);
+	
+})
 
 app.listen(port, () => console.log('Server Running'));
