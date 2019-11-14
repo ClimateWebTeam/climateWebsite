b4b77c6beb97f30c5a17078c85f79506f02374fe
Thiruisa <45204969+Thiruisa@users.noreply.github.com>
2019-11-14 16:06:49 +0000

    js update

--- JS/wp.js
+++ JS/wp.js
@@ -1,69 +1,69 @@
 
-// //define get the form values
+//define get the form values
 
-// let form = document.querySelector('form');
+let form = document.querySelector('form');
 
-// //Create a new variable to send the data to the server
+//Create a new variable to send the data to the server
 
-// form.onsubmit = sendData;
+form.onsubmit = sendData;
 
-// //New function for passing data
+//New function for passing data
 
-// function sendData(e){
-	// e.preventDefault();
+function sendData(e){
+	e.preventDefault();
 	
-	// //Possibly validate on client side here
+	//Possibly validate on client side here
 	
-	// //Create a new formData object
+	//Create a new formData object
 	
-	// let formData = new FormData(form);
+	let formData = new FormData(form);
 	
-	// //Create an object to send the data from the form in JSON format
+	//Create an object to send the data from the form in JSON format
 	
-	// let Params = {
-		// headers: {
-			// 'Content-type': 'application/json'
-		// },
-		// body: JSON..stringify({
-			// name: formData.get('name')
-			// email: formData.get('email')
-		// }),
-		// method: "POST"
-	// }
+	let Params = {
+		headers: {
+			'Content-type': 'application/json'
+		},
+		body: JSON..stringify({
+			name: formData.get('name')
+			email: formData.get('email')
+		}),
+		method: "POST"
+	}
 	
-	// //Fetch the data back from the server
+	//Fetch the data back from the server
 	
-	// fetch('http://localhost:3000/formData', Params)
-	// .then(response => response.json())
-	// .then(data => {
+	fetch('http://localhost:3000/formData', Params)
+	.then(response => response.json())
+	.then(data => {
 		
-		// //If statement to check for errors
+		//If statement to check for errors
 		
-		// if (data.success === "Okay"){
-			// console.log(Successful);
-		// }
-		// else{
-			// let error = document.querySelector('.error');
+		if (data.success === "Okay"){
+			console.log(Successful);
+		}
+		else{
+			let error = document.querySelector('.error');
 		
-			// //Reset errors ater every refresh
+			//Reset errors ater every refresh
 		
-			// error.innerHTML = "";
+			error.innerHTML = "";
 		
-			// //document.querySelector('.errorContainer').style.display = "block";
+			//document.querySelector('.errorContainer').style.display = "block";
 		
-			// data.errors.forEach(function(err){
-				// error.innerHTML += `<li>${err.msg}</li>`
-			// });
-		// }
+			data.errors.forEach(function(err){
+				error.innerHTML += `<li>${err.msg}</li>`
+			});
+		}
 		
 		
 		
-		// console.log(data);
+		console.log(data);
 		
-	// })
-	// .catch(err => console.log(err))
+	})
+	.catch(err => console.log(err))
 	
-// }
+}
 
 
 
