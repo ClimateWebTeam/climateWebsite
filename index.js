886e6812920325503c2edc701a3a3730201e5cbc
Ben Gavin-Cooper (CMP - Student) <ccg18fdu@UEA.AC.UK>
2019-11-19 15:15:43 +0000

    Added Mailer module

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 3000;
 const path = require('path');
 const bodyParser = require('body-parser');
 const { check, validationResult } = require('express-validator');
+const mail = require('nodemailer');
 
 //Not sure what this does yet
 
@@ -81,6 +82,8 @@ app.post('/formdata', [
 		success: 'Okay'
 	})
 	
+	console.log("Send Email Now")
+	
 	//Show the content of the name input in the console
 	
 	console.log(request.body.name);
@@ -92,3 +95,28 @@ app.post('/formdata', [
 //Get the application to listen on port 3000 for requests
 
 app.listen(port, () => console.log('Server Running'));
+
+
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'youremail@gmail.com',
+    pass: 'yourpassword'
+  }
+});
+
+var mailOptions = {
+  from: 'youremail@gmail.com',
+  to: 'myfriend@yahoo.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
